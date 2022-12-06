cb68f7ea12a3484672ebef572606133d0aa9f8a7
conradhale <halec5@mail.uc.edu>
2022-12-06 13:30:06 -0500

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+  private static final ArrayList<String> list = new ArrayList<>();
+  private static final Scanner in = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    String option;
+    do {
+      option = SafeInput.getRegExString(in, "Enter option [a|d|p|q]", "[AaDdPpQq]");
+      if (option.equalsIgnoreCase("a")) {
+        add();
+      }
+      if (option.equalsIgnoreCase("d") && list.size() > 0) {
+        delete();
+      }
+      if (option.equalsIgnoreCase("p")) {
+        print();
+      }
+      if (option.equalsIgnoreCase("q")) {
+        if (quit()) {
+          break;
+        }
+      }
+    } while (true);
+  }
+
+  private static void add() {
+    list.add(SafeInput.getNonZeroLenString(in, "Enter string to add to list"));
+  }
+  private static void delete() {
+    int count = 0;
+    for (String value : list) {
+      count++;
+      System.out.println(count + ": " + value);
     }
+    if (list.size() == 1) {
+      if (SafeInput.getYNConfirm(in, "Remove only index in list?")) {
+        list.remove(0);
+      }
+    } else {
+      list.remove(SafeInput.getRangedInt(in, "Enter index to remove", 1, list.size()) - 1);
+    }
+  }
+  private static void print() {
+    System.out.println(list);
+  }
+  private static boolean quit() {
+    return SafeInput.getYNConfirm(in, "Are you sure you want to quit?");
+  }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,2 +1,106 @@
-package PACKAGE_NAME;public class SafeInput {
+import java.util.Scanner;
+
+public class SafeInput {
+  /**
+   *
+   * @param pipe Scanner
+   * @param prompt Message
+   * @return String
+   */
+  public static String getNonZeroLenString(Scanner pipe, String prompt) {
+    String retString;  // Set this to zero length. Loop runs until it isn’t
+    do {
+      System.out.print(prompt + ": "); // show prompt add space
+      retString = pipe.nextLine();
+    } while (retString.length() == 0);
+
+    return retString;
+  }
+
+  /**
+   *
+   * @param pipe Scanner
+   * @param prompt String
+   * @param low int
+   * @param high int
+   * @return int
+   */
+  public static int getRangedInt(Scanner pipe, String prompt, int low, int high) {
+    int retInt = -1;
+    String trash;
+    boolean done = false;
+
+    do {
+      System.out.print(prompt + " [" + low + " - " + high + "]: ");
+      if (pipe.hasNextInt()) {
+        retInt = pipe.nextInt();
+        pipe.nextLine();
+        if (retInt < low) {
+          System.out.println("You entered " + retInt + ", which is lower than " + low + ".");
+        } else if (retInt > high) {
+          System.out.println("You entered " + retInt + ", which is higher than " + high + ".");
+        } else {
+          done = true;
+        }
+      } else {
+        trash = pipe.nextLine();
+        System.out.println("You entered " + trash + ", which is invalid.");
+      }
+    } while (!done);
+
+    return retInt;
+  }
+
+  /**
+   *
+   * @param pipe Scanner
+   * @param prompt String
+   * @return boolean
+   */
+  public static boolean getYNConfirm(Scanner pipe, String prompt) {
+    boolean retBool = false;
+    String input;
+    boolean done = false;
+
+    do {
+      System.out.print(prompt + " [y/n]: ");
+      input = pipe.nextLine();
+      if (input.equalsIgnoreCase("y")) {
+        retBool = true;
+        done = true;
+      } else if (input.equalsIgnoreCase("n")) {
+        done = true;
+      } else {
+        System.out.println("You entered " + input + ", which is invalid.");
+      }
+    } while (!done);
+
+    return retBool;
+  }
+
+  /**
+   *
+   * @param pipe Scanner
+   * @param prompt String
+   * @param regEx pattern
+   * @return String
+   */
+  public static String getRegExString(Scanner pipe, String prompt, String regEx) {
+    String retString;
+    boolean done = false;
+
+    do {
+      System.out.print(prompt + ": ");
+
+      retString = pipe.nextLine();
+
+      if (retString.matches(regEx)) {
+        done = true;
+      } else {
+        System.out.println("You entered: " + retString + ", which is invalid.");
+      }
+    } while (!done);
+
+    return retString;
+  }
 }
